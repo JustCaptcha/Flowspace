cbd0ba22efc7c7a6d1e8b0c584ee345a61f81812
JustCaptcha <higashi166@gmail.com>
2019-10-18 16:03:07 +0200

    Updates sticky footer and header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { classes, style } from './header.st.css';
-import { Typography } from '@material-ui/core';
+import { Typography, Container, Grid } from '@material-ui/core';
 import Digitflow from '../../static/svg/logos/digitflow.svg';
 import { ConsolePointer } from '../console/console';
 
@@ -10,10 +10,16 @@ export interface IHeaderProps {
 
 export const Header: React.FunctionComponent<IHeaderProps> = props => (
     <header className={style(classes.root, props.className)}>
-        <img className={style(classes.digitflowLogo)} src={Digitflow}></img>
-        <div className={style(classes.titleGroup)}>
-            <Typography className={style(classes.title)} variant='h2'>Flowspace</Typography>
-            <ConsolePointer variant='h3'/>
-        </div>
+      <Container>
+        <Grid container alignItems='center' justify='center' direction='column'>
+          <Grid container item xs={12} justify='center' alignItems='center'>
+            <img className={style(classes.digitflowLogo)} src={Digitflow}></img>
+            <div className={style(classes.titleGroup)}>
+              <Typography className={style(classes.title)} variant='h4'>Flowspace</Typography>
+              <ConsolePointer variant='h4'/>
+            </div>
+          </Grid>
+        </Grid>
+      </Container>
     </header>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/app';
-import './index.st.css';
+import { style, classes } from './index.st.css';
 import * as serviceWorker from './service-worker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App className={style(classes.root)}/>, document.getElementById('root'));
 
 // to use service worker, change to .register()
 serviceWorker.unregister();
